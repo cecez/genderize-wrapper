516d3ce1f63d8429b4495b08e596140a907ea437
Cezar Castro Rosa <cecez86@gmail.com>
2022-01-20 14:46:26 -0300

    refactor: métodos específicos para cada parte do programa (pt2)

--- src/Api.php
+++ src/Api.php
@@ -2,28 +2,15 @@
 
 namespace Cecez\GenderizeWrapper;
 
+use Exception;
+
 class Api
 {
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public static function getGender(string $name): Gender
     {
-        $objeto = new Processadora($name);
-        $objeto->sanitiza();
-        $objeto->valida();
-        $url = $objeto->urlDaRequisicao();
-
-        // enviar requisição
-        $retornoJSON = file_get_contents($url);
-
-        // processar requisição
-        if ($retornoJSON === false) throw new \Exception("Não foi possível processar requisição", 1);
-        $retornoDecodificado = json_decode($retornoJSON);
-
-        // processar resultado
-        if ($retornoDecodificado->probability <= 0.5) return Gender::UNKNOWN;
-        if ($retornoDecodificado->gender === "male") return Gender::MASCULINE;
-        return Gender::FEMININE;
+        return Processadora::executa($name);
     }
 }
--- src/Processadora.php
+++ src/Processadora.php
@@ -2,13 +2,29 @@
 
 namespace Cecez\GenderizeWrapper;
 
+use Exception;
+use JetBrains\PhpStorm\Pure;
+
 class Processadora
 {
     private readonly string $nomeSanitizado;
+    private readonly mixed $retornoDecodificado;
 
     public function __construct(
         private readonly string $nomeOriginal
     ) {}
+
+    /**
+     * @throws Exception
+     */
+    public static function executa(string $name): Gender
+    {
+        $objeto = new Processadora($name);
+        $objeto->sanitiza();
+        $objeto->valida();
+        $objeto->processaRequisicao();
+        return $objeto->processaResultado();
+    }
 
     public function sanitiza()
     {
@@ -16,22 +32,68 @@ class Processadora
     }
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
     public function valida()
     {
         if (empty($this->nomeSanitizado)) {
-            throw new \Exception("O nome não pode estar em branco.", 3);
+            throw new Exception("O nome não pode estar em branco.", 3);
         }
 
         if (count(explode(" ", $this->nomeSanitizado)) > 1) {
-            throw new \Exception("O nome não pode conter espaços.", 2);
+            throw new Exception("O nome não pode conter espaços.", 2);
         }
     }
 
-    public function urlDaRequisicao()
+    private function urlDaRequisicao(): string
     {
         $parametroNome = urlencode($this->nomeSanitizado);
         return "https://api.genderize.io?name=$parametroNome";
+    }
+
+    #[Pure] private function enviaRequisicao(): bool|string
+    {
+        // enviar requisição
+        return file_get_contents($this->urlDaRequisicao());
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function processaRequisicao()
+    {
+        // processar requisição
+        $retornoBruto = $this->enviaRequisicao();
+
+        if ($retornoBruto === false) {
+            throw new Exception("Não foi possível processar requisição", 1);
+        }
+
+        $this->retornoDecodificado = json_decode($retornoBruto);
+        if (is_null($this->retornoDecodificado)) {
+            throw new Exception("Não foi possível decodificar resultado da requisição.", 5);
+        }
+
+        if (!isset($this->retornoDecodificado->probability)) {
+            throw new Exception("Atributo de probabilidade inexistente.", 6);
+        }
+
+        if (!isset($this->retornoDecodificado->gender)) {
+            throw new Exception("Atributo de gênero inexistente.", 7);
+        }
+    }
+
+    public function processaResultado(): Gender
+    {
+        // processar resultado
+        if ($this->retornoDecodificado->probability <= 0.5) {
+            return Gender::UNKNOWN;
+        }
+
+        if ($this->retornoDecodificado->gender === "male") {
+            return Gender::MASCULINE;
+        }
+
+        return Gender::FEMININE;
     }
 }
